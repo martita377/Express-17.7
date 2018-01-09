06c0f663235245d6d7d7ce8f973bd174d73bfefc
Marta P <mapa.startuj@gmail.com>
2018-01-09 14:14:41 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '223590296697-hg37cd3rceiqb72eg735iuf7lcebdsi9.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '85sqpiImIEgfgXLqQvNIixfO',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
